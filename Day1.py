063efe0856e5011dc5548e4225377e0a657982e5
Josh Ethridge <joshua.ethridge13@gmail.com>
2019-12-01 11:41:38 -0500

    Day 1 complete

--- Day1.py
+++ Day1.py
@@ -7,7 +7,16 @@ def load(path):
 	return data
 
 def calcFuel(mass):
-	return (mass // 3) - 2
+	fuel = (mass // 3) - 2
+	if fuel < 0:
+		fuel = 0
+	return fuel
+
+def calcFuel2(mass):
+	if mass == 0:
+		return 0
+	fuel = calcFuel(mass)
+	return fuel + calcFuel2(fuel)
 
 def part1():
 	data = load('Day1.txt')
@@ -17,8 +26,17 @@ def part1():
 		sum += calcFuel(int(line))
 	return sum
 
+def part2():
+	data = load('Day1.txt')
+	data = data.split()
+	sum = 0
+	for line in data:
+		sum += calcFuel2(int(line))
+	return sum
+
 class TestDay1(unittest.TestCase):
 
+	# Part 1 Tests
 	def test1(self):
 		mass = 12
 		self.assertEqual(calcFuel(mass), 2)
@@ -35,7 +53,22 @@ class TestDay1(unittest.TestCase):
 		mass = 100756
 		self.assertEqual(calcFuel(mass), 33583)
 
+	# Part 2 Tests
+	def test5(self):
+		mass = 14
+		self.assertEqual(calcFuel2(mass), 2)
+
+	def test6(self):
+		mass = 1969
+		self.assertEqual(calcFuel2(mass), 966)
+
+	def test7(self):
+		mass = 100756
+		self.assertEqual(calcFuel2(mass), 50346)
+
 if __name__ == '__main__':
-	#unittest.main()
+	# unittest.main()
 	# Part 1: 3560353
 	print(part1())
+	# Part 2: 5337642
+	print(part2())
