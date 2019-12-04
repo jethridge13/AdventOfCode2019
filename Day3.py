217b592de1867b4b1e338be887816ed51de9faba
Josh Ethridge <jethridge@kmbs.konicaminolta.us>
2019-12-04 12:57:48 -0500

    WIP Day 3 Part 1

--- Day3.py
+++ Day3.py
@@ -8,6 +8,33 @@ def parseInstruction(ins):
 	length = int(ins[1:])
 	return (direction, length)
 
+def generateLineSegments(line):
+	segments = []
+	curIndex = [0, 0]
+	instructions = line.split(',')
+	while len(instructions) > 0:
+		# Get starting point for line
+		start = curIndex.copy()
+		# Determine ending point for line
+		ins = parseInstruction(instructions.pop(0))
+		if ins[0] == 'R':
+			curIndex[0] += ins[1]
+		elif ins[0] == 'L':
+			curIndex[0] -= ins[1]
+		elif ins[0] == 'U':
+			curIndex[1] += ins[1]
+		elif ins[0] == 'D':
+			curIndex[1] -= ins[1]
+		end = curIndex.copy()
+		segments.append((start, end))
+	return segments
+
+def checkForIntersection(line1, line2):
+	# https://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
+	result = (False, [])
+	return result
+
+
 class TestDay3(unittest.TestCase):
 
 	def test1(self):
@@ -15,6 +42,14 @@ class TestDay3(unittest.TestCase):
 
 	def test2(self):
 		self.assertEqual(parseInstruction('R9999'), ('R', 9999))
+
+	def test3(self):
+		self.assertEqual(generateLineSegments('R75'), [([0,0],[75,0])])
+
+	def test4(self):
+		inp = 'R75,D30'
+		outp = [([0,0], [75,0]), ([75,0], [75,-30])]
+		self.assertEqual(generateLineSegments(inp), outp)
 	
 if __name__ == '__main__':
 	unittest.main()
