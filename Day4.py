e6955690c1c00108cb21ede31d077a936d7ce9d7
Josh Ethridge <jethridge@kmbs.konicaminolta.us>
2019-12-10 13:02:44 -0500

    WIP Day 4 Part 1

---
+++ Day4.py
@@ -0,0 +1,64 @@
+import unittest
+import re
+
+def breakIntoRanges(range):
+	limits = range.split('-')
+	limits[0] = int(limits[0])
+	limits[1] = int(limits[1])
+	return limits
+
+def isValid(n, minVal=0, maxVal=1000000):
+	repeatedChars = '([0-9])\1'
+	increasing = '^(?=\d{6}$)0?1?2?3?4?5?6?7?8?9?'
+	match = re.search(increasing, n)
+	if not match:
+		return False
+	match = re.search(repeatedChars, n)
+	if not match:
+		return False
+	return True
+
+def part1():
+	pass
+
+def part2():
+	pass
+
+class TestDay4(unittest.TestCase):
+
+	def test1(self):
+		inp = '1-5'
+		outp = [1,5]
+		self.assertEqual(breakIntoRanges(inp), outp)
+
+	def test2(self):
+		inp = '124075-580769'
+		outp = [124075,580769]
+		self.assertEqual(breakIntoRanges(inp), outp)
+
+	def test4(self):
+		self.assertFalse(isValid('1'))
+
+	def test5(self):
+		self.assertFalse(isValid('123456'))
+
+	def test6(self):
+		self.assertFalse(isValid('654321'))
+
+	def test7(self):
+		self.assertFalse(isValid('665432'))
+
+	def test8(self):
+		self.assertFalse(isValid('112315'))
+	
+	def test9(self):
+		inp = '112345'
+		self.assertTrue(isValid(inp))
+
+if __name__ == '__main__':
+	unittest.main()
+	inp = '124075-580769'
+	# Part 1:
+	print(part1())
+	# Part 2:
+	print(part2())
